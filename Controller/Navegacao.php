46b73641f02ca85e2bf0bec76a21a8c8adde2fe5
Paulo Gustavo <64154703+Pgustavols@users.noreply.github.com>
2024-05-15 19:06:58 -0300

    Update Navegacao.php

--- Controller/Navegacao.php
+++ Controller/Navegacao.php
@@ -6,13 +6,15 @@
     switch ($_POST){
         //Caso a variavel seja nula mostrar a tela de login
         case isset($_POST[null]):
-            include_once "../TCC/View/login.php";
+            include_once "View/login.php";
             break;
 
         //Login
         case isset($_POST["btnLogin"]):
             require_once "../Controller/UsuarioController.php";
             $uController = new UsuarioController();
+
+            case isset($_POST["btnLogin"]):
 
             if(empty($_POST["txtCPF"]) || empty($_POST["txtSenha"])){
                 echo
@@ -21,6 +23,7 @@
                 window.history.back();
                 </script>";
                 } else {
+                    $uController = new UsuarioController();
                     $result = $uController->login($_POST["txtCPF"], $_POST["txtSenha"]);
 
                     if($result == 1){
@@ -35,6 +38,5 @@
                     }
                 }
                 break;
-
     }
 ?>
